6aca562db30245ced531f8ce4e1d9176fa29e89a
Nuke <flow.energy@gmail.com>
2012-06-05 16:37:23 +0400

    fixes

--- app/assets/javascripts/ready.js
+++ app/assets/javascripts/ready.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
     $("ul.gallery li").hover(function() {
         var thumbOver = $(this).find("img").attr("src");
-        $(this).find("img").stop().fadeTo('normal', 0.5,
-        function() {
-            $(this).hide()
-        });				
+        $(this).find("img").stop().fadeTo('normal', 0.5);				
     } , function() {
-        $(this).find("img").stop().fadeTo('normal', 1).show();			
+        $(this).find("img").stop().fadeTo('normal', 1);			
     });						
 });
