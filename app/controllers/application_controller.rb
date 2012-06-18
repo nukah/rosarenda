97fce6d3762b2e4c43c0dd28257e9b3ff6e3d0f3
Nuke <flow.energy@gmail.com>
2012-06-18 12:53:23 +0400

    404 fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   def error_404
     respond_to do |type| 
-      type.html { render :template => "utility/notfound", :layout => 'application', :status => 404, :locals => {:ref => request.referrer } } 
+      type.html { render :template => "utility/notfound", :layout => 'application', :status => 404, :locals => { @ref => request.referrer } } 
       type.all  { render :nothing => true, :status => 404 } 
     end
     true
