d7c2d37f20e81001035df52f00ed10f82cb0808e
Nuke <flow.energy@gmail.com>
2014-10-02 15:22:37 +0400

    updates

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,11 +4,10 @@ class MainController < ApplicationController
   def index
     redirect_to '/objects'
   end
-  
+
   def subscribe
     email = params[:email].to_s
-    digest = Digest::SHA256.hexdigest(email.to_s + rand(10**15).to_s)
-    @record = Subscriber.new(:email => email, :code => digest.to_s)
+    @record = Subscriber.new(:email => email)
     if @record.save
       render
     end
@@ -17,11 +16,11 @@ class MainController < ApplicationController
   def unsubscribe
     confirm = params[:code].to_s
     @sub = Subscriber.where(:code => confirm).first
-        
+
     if @sub.present?
       @sub.delete and render
     else
       render 'main/invalid' if not @sub
-    end  
+    end
   end
 end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,8 +1,14 @@
 class Subscriber < ActiveRecord::Base
-  attr_accessible :code, :email, :name
+  attr_accessible :email, :name
+  before_save :generate_token
   validates :name, :presence => true
   validates :email, :presence => true,
                       :length => {:minimum => 3, :maximum => 254},
                       :uniqueness => true,
                       :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
+  private
+
+  def generate_token
+    code = Digest::SHA256.hexdigest(email.to_s + rand(10**15).to_s)
+  end
 end
